9458f4abc9e21c152a60a3a52e7af5ffbf037f25
Sriragbadveti <badvetisrirag@gmail.com>
2025-11-23 11:29:39 +0400

    final changes

--- src/lib/axios.instance.jsx
+++ src/lib/axios.instance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL || "https://whisppp-backend.onrender.com",
     withCredentials:true
 })
--- src/stores/useAuthStore.jsx
+++ src/stores/useAuthStore.jsx
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.instance";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://whisppp-backend.onrender.com";
 export const useAuthStore = create((set, get) => ({
   authUser: null,
   isChecking: true,
