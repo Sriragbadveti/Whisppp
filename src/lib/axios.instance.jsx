09ca9db09646083813dcbb1a00ec6a0ab6b0babb
Sriragbadveti <badvetisrirag@gmail.com>
2025-11-23 11:38:01 +0400

    done

--- src/lib/axios.instance.jsx
+++ src/lib/axios.instance.jsx
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "https://whisppp-backend.onrender.com",
-    withCredentials:true
+    withCredentials: true
 })
